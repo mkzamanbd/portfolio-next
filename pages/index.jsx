d7757888ae1987c0ab8b54001658c66551c862fa
Kamruzzaman <zaman7u@gmail.com>
2021-07-23 21:34:28 +0600

    Add global meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,9 @@
-import Head from 'next/head'
 import TopHeader from './../components/TopHeader'
-import favicon from '../public/favicon.png'
 
 export default function Home() {
     return (
         <div>
-            <Head>
-                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-                <meta name="keywords" content="web development, web developer, Laravel, php, Vue js, Nuxt js, React js, Next js, bootstrap, website, kamruzzaman, web design, landing page, interaction design, sketch" />
-                <meta name="description" content="Transforming idea from designing to client side coding for web, Designing user interface for web & mobile platforms..." />
-                <meta name="author" content="kamruzzaman" />
-                <meta name="copyright" content="kamruzzaman" />
-                <meta name="robots" content="index, follow" />
-                <meta name="theme-color" content="#0180FF" />
-                <title>Kamruzzaman | Web & Android Application Developer</title>
-                <link rel="icon" href={favicon.src} type="image/x-icon" />
-                <meta property="og:image" content="/images/zaman.png" />
-                <meta property="og:image:secure_url" content="/images/zaman.png" />
-                <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:width" content="420" />
-                <meta property="og:image:height" content="280" />
-                <meta property="og:image:alt" content="I'm Zaman" />
-
-            </Head>
-
             <TopHeader />
-
         </div>
     )
 }
